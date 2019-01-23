66f745062a68b00e1109b732efb9baa4c76d26f8
Rudax <rudycollet@sfr.fr>
2019-01-23 17:00:29 +0100

    version rudy

--- ppe2/Vues/menu.php
+++ ppe2/Vues/menu.php
@@ -1,110 +1,115 @@
 <header>
 	<img class="logo" src="img/aussonne.png" alt="">
-	<ul id="menu-bar">
-		<li>
-			<a href="#">CLUBS</a>
-			<ul>
-				<li>
-					<a href = 'index.php?vue=club&action=ajouter'>Ajouter un Club</a>
-				</li>
-				<li>
-					<a href = 'index.php?vue=club&action=visualiser'>Voir les Clubs</a>
-				</li>
-				<li>
-					<a href="construction.php">
-						<font color=#908E8E>Modifier un Club</font>
-					</a>
-				</li>
-			</ul>
-		</li>
-		<li>
-			<a href="#" class="afficher">RESSOURCES</a>
-			<ul>
-				<li>
-					<a href = 'index.php?vue=ressource&action=ajouter'>Ajouter une Ressource</a>
-				</li>
-				<li>
-					<a  href = 'index.php?vue=ressource&action=visualiser'>Voir les Ressources</a>
-				</li>
-				<li>
-					<a href="construction.php">
-						<font color=#908E8E>Modifier une Ressource</font>
-					</a>
-				</li>
-			</ul>
-		</li>
-
-		<li>
-			<a href="#">EQUIPES</a>
-			<ul>
-				<li>
-					<a href="construction.php">
-						<font color=#908E8E>Ajouter une equipe</font>
-					</a>
-				</li>
-				<li>
-					<a href="construction.php">
-						<font color=#908E8E>Voir une Equipe</font>
-					</a>
-				</li>
-				<li>
-					<a href="construction.php">
-						<font color=#908E8E>Modifier une Equipe</font>
-					</a>
-				</li>
-			</ul>
-		</li>
-		<li>
-			<a href="#">JOUR SEMAINE</a>
-			<ul>
-				<li>
-					<a href="construction.php">
-						<font color=#908E8E>Ajouter un Jour</font>
-					</a>
-				</li>
-				<li>
-					<a href="construction.php">
-						<font color=#908E8E>Voir un Jour</font>
-					</a>
-				</li>
-				<li>
-					<a href="construction.php">
-						<font color=#908E8E>Modifier un Jour</font>
-					</a>
-				</li>
-				<li>
-					<a href="construction.php">
-						<font color=#908E8E>Voir les Equipes de la journée</font>
-					</a>
-				</li>
-			</ul>
-		</li>
-		<li>
-			<a href="#">ENTRAINEURS</a>
-			<ul>
-				<li>
-					<a href="construction.php">
-						<font color=#908E8E>Ajouter un Entraineur</font>
-					</a>
-				</li>
-				<li>
-					<a href="construction.php">
-						<font color=#908E8E>Voir un Entraineur</font>
-					</a>
-				</li>
-				<li>
-					<a href="construction.php">
-						<font color=#908E8E>Modifier un Entraineur</font>
-					</a>
-				</li>
-				<li>
-					<a href="construction.php">
-						<font color=#908E8E>Voir les Equipes de l'entraineur</font>
-					</a>
-				</li>
-			</ul>
-		</li>
-	</ul>
+	<div class="menu">
+		<div class="pos-f-t">
+	  	<div class="collapse" id="navbarToggleExternalContent">
+	    	<div class="bg-blue p-4">
+					<span class="text-muted">CLUBS
+						<ul>
+							<li>
+								<a href = 'index.php?vue=club&action=ajouter'>Ajouter un Club</a>
+							</li>
+							<li>
+								<a href = 'index.php?vue=club&action=visualiser'>Voir les Clubs</a>
+							</li>
+							<li>
+								<a href="construction.php">
+									<font color=#908E8E>Modifier un Club</font>
+								</a>
+							</li>
+						</ul>
+					</span>
+					<span class="text-muted">RESSOURCES
+						<ul>
+							<li>
+								<a href = 'index.php?vue=ressource&action=ajouter'>Ajouter une Ressource</a>
+							</li>
+							<li>
+								<a  href = 'index.php?vue=ressource&action=visualiser'>Voir les Ressources</a>
+							</li>
+							<li>
+								<a href="construction.php">
+									<font color=#908E8E>Modifier une Ressource</font>
+								</a>
+							</li>
+						</ul>
+					</span>
+					<span class="text-muted">EQUIPES
+						<ul>
+							<li>
+								<a href="construction.php">
+									<font color=#908E8E>Ajouter une equipe</font>
+								</a>
+							</li>
+							<li>
+								<a href="construction.php">
+									<font color=#908E8E>Voir une Equipe</font>
+								</a>
+							</li>
+							<li>
+								<a href="construction.php">
+									<font color=#908E8E>Modifier une Equipe</font>
+								</a>
+							</li>
+						</ul>
+					</span>
+					<span class="text-muted">JOUR SEMAINE
+						<ul>
+							<li>
+								<a href="construction.php">
+									<font color=#908E8E>Ajouter un Jour</font>
+								</a>
+							</li>
+							<li>
+								<a href="construction.php">
+									<font color=#908E8E>Voir un Jour</font>
+								</a>
+							</li>
+							<li>
+								<a href="construction.php">
+									<font color=#908E8E>Modifier un Jour</font>
+								</a>
+							</li>
+							<li>
+								<a href="construction.php">
+									<font color=#908E8E>Voir les Equipes de la journée</font>
+								</a>
+							</li>
+						</ul>
+					</span>
+					<span class="text-muted">ENTRAINEURS
+						<ul>
+							<li>
+								<a href="construction.php">
+									<font color=#908E8E>Ajouter un Entraineur</font>
+								</a>
+							</li>
+							<li>
+								<a href="construction.php">
+									<font color=#908E8E>Voir un Entraineur</font>
+								</a>
+							</li>
+							<li>
+								<a href="construction.php">
+									<font color=#908E8E>Modifier un Entraineur</font>
+								</a>
+							</li>
+							<li>
+								<a href="construction.php">
+									<font color=#908E8E>Voir les Equipes de l'entraineur</font>
+								</a>
+							</li>
+						</ul>
+					</span>
+  			</div>
+			</div>
+			<nav class="navbar navbar-dark bg-dark">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+	    		<span class="navbar-toggler-icon"></span>
+   			</button>
+	  	</nav>
+		</div>
+	</div>
 </header>
 		<!--<li><a href="#">ADHERENTS</a>
 		<ul>
