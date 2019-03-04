0d0b0f0e686c5116d38663dfb869e7b981f73bd6
Augustin Ribreau <augustin@fgribreau.com>
2019-03-04 14:25:14 +0100

    Mission 2 - 2.4.6 : remplacement du username et passage du SMTP en false

--- ppe2/ajax/valide_mp_perdu.php
+++ ppe2/ajax/valide_mp_perdu.php
@@ -33,14 +33,14 @@ if ($result)
 			$mail->isSMTP();                                      // Set mailer to use SMTP
 			$mail->Host = '127.0.0.1';  // Specify main and backup SMTP servers
 			// $mail->Host = 'smtp.gmail.com';
-			$mail->SMTPAuth = true;                               // Enable SMTP authentication
+			$mail->SMTPAuth = false;                               // Enable SMTP authentication
 			$mail->Username = 'useriw';                 // SMTP username
 			$mail->Password = 'useriw';                           // SMTP password
 			// $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 			$mail->Port = 25;                                    // TCP port to connect to
 
 			//Recipients
-			$mail->setFrom('pfromeuf@gmail.com');
+			$mail->setFrom('useriw');
 			$mail->addAddress($tab['id'], 'AUSSONNE-ID');     // Add a recipient
 			// $mail->addAddress('pfromeuf@gmail.com', 'AUSSONNE-ID');     // Add a recipient
 			//$mail->addReplyTo($_POST['mail']);
